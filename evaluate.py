cba67175245be663615bee232f44a1e083b2814f
Jeroen Offerijns <jeroen@offerijns.com>
2019-12-02 22:27:37 +0100

    chore: remove debugging code

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,7 @@
 from algorithms.hyperband import Hyperband
 from models.cifar_cnn import CifarCnn
 
-import faulthandler
-
 model = CifarCnn()
 hb = Hyperband(model, 10, 3)
 
-faulthandler.enable()
 hb.tune()
